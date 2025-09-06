2bdbdc3dc269a820a5be8b606c49952c276f84f1
kunkaffa <kunkaffa3@gmail.com>
2025-09-06 10:04:55 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,17 +2,11 @@ import os
 c = input("Choose your environment: [0] pip / [1] pip3 : ")
 
 if c == "0":
-    os.system("apt-get install python")
-    os.system("apt-get install python2")
-    os.system("apt-get install python3-pip")
     os.system("pip install requests")
     os.system("pip install asyncio")
     os.system("pip install aiohttp")
     os.system("pip install fade")
 elif c == "1":
-    os.system("apt-get install python")
-    os.system("apt-get install python2")
-    os.system("apt-get install python3-pip")
     os.system("pip3 install requests")
     os.system("pip3 install asyncio")
     os.system("pip3 install aiohttp")
